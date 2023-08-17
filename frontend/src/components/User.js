ffbc26f1c8404295ab663105f96351f3b77a7e1c
Somesh Naruka <someshnaruka@gmail.com>
2023-08-17 18:02:36 +0530

    fixed uploading loader in profile img upload and added read me

--- frontend/src/components/User.js
+++ frontend/src/components/User.js
@@ -6,6 +6,7 @@ import { ImgtoBase64 } from "../utility/ImgtoBase64";
 import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import { toast } from "react-hot-toast";
+import { RotatingLines } from "react-loader-spinner";
 import {
   AddcertiRedux,
   AddeduRedux,
@@ -35,6 +36,7 @@ function User(props) {
   const dispatch = useDispatch();
   const [edit, setEdit] = useState(false);
   const [img, setImg] = useState("");
+  const [isLoading, setLoading] = useState(false);
   const [imgValue, setValue] = useState({
     _id: data[0]._id,
     image: "",
@@ -63,36 +65,38 @@ function User(props) {
         [name]: value,
       };
     });
- 
   }
   const [change, setChange] = useState([]);
 
   const token = localStorage.getItem("dashboardToken");
 
-  useEffect(()=>{
-   
+  useEffect(() => {
     handlePhoto();
+  }, [imgValue]);
 
-  },[imgValue])
-
-  function handlePhoto(){
-      axios.patch(process.env.REACT_APP_SERVER_DOMAIN + "/profileImg", imgValue, {
-          headers: {
-            Authorization: token,
-          },
-        }).then((response)=>{
-          
-if(response.data.alert){
- 
-  toast(response.data.message)
-  dispatch(imageRedux(response.data.result))
-}
-            
-      
-        }).catch((err)=>{
-          console.log(err);
-        })
+  function handlePhoto() {
+    axios
+      .patch(process.env.REACT_APP_SERVER_DOMAIN + "/profileImg", imgValue, {
+        headers: {
+          Authorization: token,
+        },
+      })
+      .then((response) => {
+        if (response.data.alert) {
+          toast(response.data.message);
+          dispatch(imageRedux(response.data.result));
+        }
+      })
+      .catch((err) => {
+        setLoading(false);
+        console.log(err);
+      })
+      .finally(() => {
+        setLoading(false);
+        
+      });
   }
+ 
 
   function handleAbout(event) {
     const { name, value } = event.target;
@@ -103,7 +107,6 @@ if(response.data.alert){
         [name]: value,
       };
     });
-  
   }
 
   function handleUpdatebio() {
@@ -114,7 +117,6 @@ if(response.data.alert){
         },
       })
       .then((response) => {
-       
         if (response.data.alert) {
           toast(response.data.message);
           dispatch(aboutRedux(about));
@@ -143,7 +145,6 @@ if(response.data.alert){
         }
       )
       .then((response) => {
-      
         if (response.data.alert) {
           toast(response.data.message);
           dispatch(AddskillRedux(newskill));
@@ -159,7 +160,6 @@ if(response.data.alert){
 
   function handleNewskill(event) {
     setNewskill(event.target.value);
-   
   }
 
   function handleprofile() {
@@ -170,7 +170,6 @@ if(response.data.alert){
         },
       })
       .then((response) => {
-       
         if (response.data.alert) {
           toast(response.data.message);
           dispatch(profileRedux(user));
@@ -184,30 +183,23 @@ if(response.data.alert){
 
   function handleEditData(value) {
     setChange([value]);
-    
   }
 
   function handleEdit(value) {
     setEdit(value);
   }
   async function handleUpload(event) {
-   
     const imgurl = await ImgtoBase64(event.target.files[0]);
-
-   
-
-   
     setValue((prevValue) => {
       return {
         ...prevValue,
         image: imgurl,
       };
     });
-   
+    setLoading(true);
+    
   }
-  
-
-
+ 
   function handleCertiUpdate(value) {
     const certiData = {
       _id: data[0]._id,
@@ -221,7 +213,6 @@ if(response.data.alert){
       ],
     };
 
-   
     axios
       .patch(process.env.REACT_APP_SERVER_DOMAIN + "/profileData", certiData, {
         headers: {
@@ -229,7 +220,6 @@ if(response.data.alert){
         },
       })
       .then((response) => {
-      
         if (response.data.alert) {
           toast(response.data.message);
           dispatch(AddcertiRedux(value));
@@ -258,7 +248,6 @@ if(response.data.alert){
         },
       })
       .then((response) => {
-       
         if (response.data.alert) {
           toast(response.data.message);
           setEdit(false);
@@ -290,7 +279,6 @@ if(response.data.alert){
         }
       )
       .then((response) => {
-       
         if (response.data.alert) {
           toast(response.data.message);
           setEdit(false);
@@ -302,7 +290,6 @@ if(response.data.alert){
       });
   }
   function handleExpUpdate(value) {
-   
     const expData = {
       _id: data[0]._id,
       experience: [...data[0].experience, value],
@@ -315,7 +302,6 @@ if(response.data.alert){
         },
       })
       .then((response) => {
-        
         if (response.data.alert) {
           toast(response.data.message);
           dispatch(AddexpRedux(value));
@@ -341,7 +327,6 @@ if(response.data.alert){
         },
       })
       .then((response) => {
-     
         if (response.data.alert) {
           toast(response.data.message);
           dispatch(AddeduRedux(value));
@@ -370,7 +355,6 @@ if(response.data.alert){
         },
       })
       .then((response) => {
-        
         if (response.data.alert) {
           toast(response.data.message);
           setEdit(false);
@@ -398,7 +382,6 @@ if(response.data.alert){
         },
       })
       .then((response) => {
-      
         if (response.data.alert) {
           toast(response.data.message);
           setEdit(false);
@@ -425,7 +408,6 @@ if(response.data.alert){
         },
       })
       .then((response) => {
-       
         if (response.data.alert) {
           toast(response.data.message);
           setEdit(false);
@@ -453,7 +435,6 @@ if(response.data.alert){
         },
       })
       .then((response) => {
-       
         if (response.data.alert) {
           toast(response.data.message);
           setEdit(false);
@@ -484,7 +465,6 @@ if(response.data.alert){
         }
       )
       .then((response) => {
-     
         if (response.data.alert) {
           toast(response.data.message);
           setEdit(false);
@@ -503,7 +483,7 @@ if(response.data.alert){
 
   return (
     <>
-      <div className=" flex md:flex-row flex-col justify-start items-start p-3 gap-14 bg-white border-[2px] rounded-lg border-[#EBEBEE] absolute  md:w-[70%] w-[90%] md:top-80 top-40 md:right-[9%] ">
+    <div className=" flex md:flex-row flex-col justify-start items-start p-3 gap-14 bg-white border-[2px] rounded-lg border-[#EBEBEE] absolute  md:w-[70%] w-[90%] md:top-80 top-40 md:right-[9%] ">
         <div className="md:w-1/2 w-full h-full flex flex-col justify-start gap-6  items-start">
           <div className="flex justify-between items-center ">
             <div className="w-28 h-28 md:w-32 md:h-32 rounded-full overflow-hidden">
@@ -512,8 +492,18 @@ if(response.data.alert){
             <div className="w-1/2">
               <label htmlFor="image">
                 <p className="text-[#373B5C] bg-[#F0EFFA] border-[1px] rounded-2xl px-3 font-body font-medium text-sm  py-2 cursor-pointer">
-                  Upload Photo
+                {
+                  isLoading ?   <div role="status">
+    <svg aria-hidden="true" className="inline w-6 h-6 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-[#1E2875]" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+    </svg>
+    <span>Uploading</span>
+</div> : <span>Upload photo</span>
+                }
+                  
                 </p>
+
                 <input
                   type={"file"}
                   id="image"
@@ -572,7 +562,16 @@ if(response.data.alert){
                     ></input>
                   </div>
                 ) : (
-                  <p> {`${data[0].firstname.charAt(0).toUpperCase()}${data[0].firstname.slice(1)} ${data[0].lastname.charAt(0).toUpperCase()}${data[0].lastname.slice(1)}`}</p>
+                  <p>
+                    {" "}
+                    {`${data[0].firstname
+                      .charAt(0)
+                      .toUpperCase()}${data[0].firstname.slice(
+                      1
+                    )} ${data[0].lastname
+                      .charAt(0)
+                      .toUpperCase()}${data[0].lastname.slice(1)}`}
+                  </p>
                 )}
               </div>
             </div>
@@ -611,7 +610,8 @@ if(response.data.alert){
                 </p>
 
                 <p className="text-[#413B89] font-display font-medium md:text-xl text-lg">
-                  {data[0].firstname.charAt(0).toUpperCase() + data[0].firstname.slice(1)}
+                  {data[0].firstname.charAt(0).toUpperCase() +
+                    data[0].firstname.slice(1)}
                 </p>
               </div>
               {edit === "bio" ? (
@@ -1053,6 +1053,8 @@ if(response.data.alert){
           </div>
         </div>
       </div>
+ 
+     
     </>
   );
 }
